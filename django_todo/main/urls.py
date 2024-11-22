85cd139b9eff7a382f0e850d2950854fe77100a1
LucLor06 <LukeLorenzBA@gmail.com>
2024-11-22 16:39:57 -0500

    Add CreateView for Task model along with urls

--- django_todo/main/urls.py
+++ django_todo/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.TaskListView.as_view())
+    path('', views.TaskListView.as_view(), name='tasks'),
+    path('create/', views.TaskCreateView.as_view(), name='tasks-create')
 ]
--- django_todo/main/views.py
+++ django_todo/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 
@@ -10,3 +11,12 @@ class TaskListView(ListView, LoginRequiredMixin):
     
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
+    
+class TaskCreateView(CreateView, LoginRequiredMixin):
+    model = Task
+    fields = ['task']
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
