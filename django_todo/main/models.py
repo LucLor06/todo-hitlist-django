d9865cd3711e92244a4210fa9f63c6c3edfbbd49
LucLor06 <LukeLorenzBA@gmail.com>
2024-11-21 22:42:03 -0500

    Create Task model

--- django_todo/main/models.py
+++ django_todo/main/models.py
@@ -3,3 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     ...
+    
+class Task(models.Model):
+    user = models.ForeignKey('User', related_name='tasks', on_delete=models.CASCADE)
+    created_on = models.DateField(auto_now_add=True)
+    task = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return self.task
+    
