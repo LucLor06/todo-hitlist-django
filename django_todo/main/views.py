c643b65cffa58be9831d17b8f751be05c56ea4c7
LucLor06 <LukeLorenzBA@gmail.com>
2024-11-22 13:14:19 -0500

    Create ListView for Task model along with its urls and templates

--- django_todo/main/views.py
+++ django_todo/main/views.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Task
 
+class TaskListView(ListView, LoginRequiredMixin):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'home.html'
+    
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
