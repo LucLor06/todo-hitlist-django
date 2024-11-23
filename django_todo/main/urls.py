17e4edecf022d66f3c4dff666bf02b007ac3eaa1
LucLor06 <LukeLorenzBA@gmail.com>
2024-11-23 16:21:42 -0500

    Add DeleteView for Task model along with respective url and template changes

--- django_todo/main/urls.py
+++ django_todo/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.TaskListView.as_view(), name='tasks'),
-    path('create/', views.TaskCreateView.as_view(), name='tasks-create')
+    path('create/', views.TaskCreateView.as_view(), name='tasks-create'),
+    path('delete/<pk>/', views.TaskDeleteView.as_view(), name='tasks-delete')
 ]
--- django_todo/main/views.py
+++ django_todo/main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 
-class TaskListView(ListView, LoginRequiredMixin):
+class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'home.html'
@@ -12,7 +12,8 @@ class TaskListView(ListView, LoginRequiredMixin):
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
     
-class TaskCreateView(CreateView, LoginRequiredMixin):
+    
+class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
     fields = ['task']
     success_url = reverse_lazy('tasks')
@@ -20,3 +21,11 @@ class TaskCreateView(CreateView, LoginRequiredMixin):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+    
+    
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    success_url = reverse_lazy('tasks')
+    
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
