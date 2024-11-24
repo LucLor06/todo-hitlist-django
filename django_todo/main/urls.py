61890d188303b737b2cd3651c4d9684f975390b6
LucLor06 <LukeLorenzBA@gmail.com>
2024-11-24 14:15:06 -0500

    Change TaskDeleteView url to follow conventions

--- django_todo/main/urls.py
+++ django_todo/main/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.TaskListView.as_view(), name='tasks'),
     path('create/', views.TaskCreateView.as_view(), name='tasks-create'),
-    path('delete/<pk>/', views.TaskDeleteView.as_view(), name='tasks-delete')
+    path('<pk>/delete/', views.TaskDeleteView.as_view(), name='tasks-delete')
 ]
